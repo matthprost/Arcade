e9d407a47c176b51bb6f80e1016331507a8278b2
Matthias Prost <matthias.prost@epitech.eu>
2017-03-17 18:48:17 +0100

    Gros refactor du gamecore

--- sources/GameCore.cpp
+++ sources/GameCore.cpp
@@ -5,7 +5,7 @@
 // Login   <loic.lopez@epitech.eu>
 //
 // Started on  jeu. mars 16 18:01:13 2017 Loïc Lopez
-// Last update Fri Mar 17 17:38:12 2017 Matthias Prost
+// Last update Fri Mar 17 18:47:35 2017 Matthias Prost
 //
 
 #include "GameCore.hpp"
@@ -24,27 +24,45 @@ GameCore& GameCore::operator=(GameCore const &controller)
   return (*this);
 }
 
+void *GameCore::openLibrary(char const *libraryName)
+{
+  void  *file;
+
+  file = Cencapsulation::c_dlopen(libraryName, RTLD_LAZY);
+  if (!file)
+    {
+      std::cerr << Cencapsulation::c_dlerror() << '\n';
+      exit(EXIT_FAILURE);
+    }
+  return (file);
+}
+
+play_function_type GameCore::getPlayFunction(void *game)
+{
+  IGameModel *(*play_function)(std::string const &);
+
+  play_function = reinterpret_cast<IGameModel *(*)(std::string const &)>(Cencapsulation::c_dlsym(game, "Play"));
+  if (!play_function)
+    {
+      std::cerr << Cencapsulation::c_dlerror() << '\n';
+      exit(EXIT_FAILURE);
+    }
+  return (play_function);
+}
+
 void GameCore::GameLauncher()
 {
-  IAssistant *(*external_func)(); // Pointer of function of funcName
-  void *handle;
+  std::vector<void *> Games;
+  std::vector<void *> Libs;
+  IGameModel *(*play_function)(std::string const &);
 
-  if (libName.empty() || funcName.empty())
-    return (-1);
-  handle = dlopen(libName.c_str(), RTLD_LAZY);
-  if (!handle)
-    {
-      dlerror();
-      return (-1);
-    }
-  external_func = reinterpret_cast<IAssistant *(*)()>(dlsym(handle, funcName.c_str()));
-  if (!external_func)
-    {
-      dlerror();
-      return (-1);
-    }
-  IAssistant *bob = external_func();
-  bob->talk() // Call the function talk()
-  dlclose(handle);
-  return (0);
+
+  Libs.push_back(this->openLibrary(this->library_name.c_str()));
+  Games.push_back(this->openLibrary("./lib/snake.so"));
+  play_function = this->getPlayFunction(Games.at(0));
+  IGameModel *game = play_function(this->library_name.c_str());
+
+  game->getInputs();
+  Cencapsulation::c_dlclose(Libs.at(0));
+  Cencapsulation::c_dlclose(Games.at(0));
 }
