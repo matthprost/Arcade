08d933a8b850c6c1aa418d62564ef577d7b73c65
Loïc Lopez <loic.lopez@epitech.eu>
2017-03-17 13:53:01 +0100

    Changement de jeu

--- games-include/Snake/Snake.hpp
+++ games-include/Snake/Snake.hpp
@@ -1,14 +1,14 @@
 /*
-** Skake.hpp.hpp for cpp_arcade in /home/lopez_i/cpp_arcade/Skake.hpp.hpp
+** Pacman.hpp for cpp_arcade in /home/lopez_i/cpp_arcade/Pacman.hpp
 **
 ** Made by Loïc Lopez
 ** Login   <loic.lopez@epitech.eu>
 **
-** Started on  jeu. mars 16 16:02:17 2017 Loïc Lopez
-** Last update jeu. mars 16 16:02:17 2017 Loïc Lopez
+** Started on  jeu. mars 16 16:01:26 2017 Loïc Lopez
+** Last update jeu. mars 16 16:01:26 2017 Loïc Lopez
 */
 
-#ifndef SNAKE_HPP__
-# define SNAKE_HPP__
+#ifndef PACMAN_HPP__
+# define PACMAN_HPP__
 
-#endif /* SNAKE_HPP__ */
+#endif /* PACMAN_HPP__ */
---
+++ games-include/SpaceFighter/SpaceFighter.hpp
@@ -0,0 +1,14 @@
+/*
+** SpaceFighter.hpp for cpp_arcade in /home/lopez_i/cpp_arcade/SpaceFighter.hpp.hpp
+**
+** Made by Loïc Lopez
+** Login   <loic.lopez@epitech.eu>
+**
+** Started on  jeu. mars 16 16:02:17 2017 Loïc Lopez
+** Last update jeu. mars 16 16:02:17 2017 Loïc Lopez
+*/
+
+#ifndef SPACEFIGHTER_HPP__
+# define SPACEFIGHTER_HPP__
+
+#endif /* SPACEFIGHTER_HPP__ */
