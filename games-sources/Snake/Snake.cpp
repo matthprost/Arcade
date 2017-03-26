0f965a6f2a8f66d3acc05f847ce485b087b545a5
Loïc Lopez <loic.lopez@epitech.eu>
2017-03-26 21:58:35 +0200

    collisions

--- games-sources/Snake/Snake.cpp
+++ games-sources/Snake/Snake.cpp
@@ -31,8 +31,8 @@ Snake::Snake(std::string const &libname)
   this->Map->type = arcade::CommandType::PLAY;
   for (int i = 0; i < this->Map->height * this->Map->width; ++i)
       this->Map->tile[i] = arcade::TileType::EMPTY;
-  pos.x = this->Map->width / 2;
-  pos.y = this->Map->height / 2;
+  pos.x = (uint16_t) (this->Map->width / 2);
+  pos.y = (uint16_t) (this->Map->height / 2);
   for (int i = 0; i < 4; ++i)
     {
       this->_snake.push_back(pos);
@@ -100,8 +100,34 @@ void  Snake::wait_second()
 
   ticks1 = clock();
   ticks2 = ticks1;
-  while ((ticks2 / (CLOCKS_PER_SEC / 1000) - ticks1 / (CLOCKS_PER_SEC / 1000)) < 125)
+  while ((ticks2 / (CLOCKS_PER_SEC / 1000) - ticks1 / (CLOCKS_PER_SEC / 1000)) < 50)
     ticks2 = clock();
+}
+
+static	bool headIsOnAWallOrSelf(arcade::GetMap *map, uint16_t head_x_pos, uint16_t head_y_pos, std::vector<arcade::Position> snake)
+{
+  int   			i;
+  int 			lenght = map->width * map->height;
+
+  i = -1;
+  while (++i < lenght)
+    {
+      if ((map->tile[i] == arcade::TileType::BLOCK && head_x_pos == 0) ||
+       (map->tile[i] == arcade::TileType::BLOCK && head_y_pos == 0) ||
+       (map->tile[i] == arcade::TileType::BLOCK && head_y_pos == map->height - 1) ||
+       (map->tile[i] == arcade::TileType::BLOCK && head_x_pos == map->width - 1))
+	return (true);
+    }
+  (void)snake;
+  // a taf
+  /*
+  for (size_t j = 1; j < snake.size(); ++j)
+    {
+      if (head_x_pos == snake.at(j).x)
+	return (true);
+    }
+    */
+  return (false);
 }
 
 bool	Snake::play(ILibraryViewController *libraryInstance,
@@ -116,8 +142,15 @@ bool	Snake::play(ILibraryViewController *libraryInstance,
   this->setMap();
   while (libraryInstance->getKey(&this->Map->type, action, exit))
     {
+      if (headIsOnAWallOrSelf(this->Map, this->_snake.at(0).x, this->_snake.at(0).y, this->_snake))
+	{
+	  libraryInstance->endScreen();
+	  std::cout << "YOU LOOSE BITCH" << std::endl;
+	  return (false);
+	}
+
       if (this->Map->type != arcade::CommandType::PLAY)
-      	for (int j = _snake.size() - 1; j > 0 ; j--)
+      	for (size_t j = _snake.size() - 1; j > 0 ; j--)
 	  {
 	    this->_snake.at(j).x = this->_snake.at(j - 1).x;
 	    this->_snake.at(j).y = this->_snake.at(j - 1).y;
@@ -135,26 +168,26 @@ bool	Snake::play(ILibraryViewController *libraryInstance,
       else if ((this->Map->type == arcade::CommandType::GO_UP && i != 2) ||
       (this->Map->type == arcade::CommandType::GO_DOWN && i == 1))
         {
-	        this->_snake.at(0).y--;
-          i = 1;
+	  this->_snake.at(0).y--;
+	  i = 1;
         }
       else if ((this->Map->type == arcade::CommandType::GO_DOWN && i != 1) ||
       (this->Map->type == arcade::CommandType::GO_UP && i == 2))
         {
-	        this->_snake.at(0).y++;
+	  this->_snake.at(0).y++;
           i = 2;
         }
       else if ((this->Map->type == arcade::CommandType::GO_LEFT && i != 4) ||
       (this->Map->type == arcade::CommandType::GO_RIGHT && i == 3))
         {
-	        this->_snake.at(0).x--;
+	  this->_snake.at(0).x--;
           i = 3;
         }
       else if ((this->Map->type == arcade::CommandType::GO_RIGHT && i != 3) ||
       (this->Map->type == arcade::CommandType::GO_LEFT && i == 4))
         {
-	        this->_snake.at(0).x++;
-          i = 4;
+	  this->_snake.at(0).x++;
+	  i = 4;
         }
       this->drawMap(libraryInstance);
       libraryInstance->displayText(this->getGameName(), libraryInstance->getLibraryName());
