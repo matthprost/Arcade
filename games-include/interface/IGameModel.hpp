f32b847b0566f6cfe4c1dece3f6f5e094c8df311
Loïc Lopez <loic.lopez@epitech.eu>
2017-04-02 20:30:06 +0200

    ok

--- games-include/interface/IGameModel.hpp
+++ games-include/interface/IGameModel.hpp
@@ -14,6 +14,7 @@
 #include <array>
 #include <map>
 #include <string>
+#include <unistd.h>
 #include "../../lib-include/interface/ILibraryViewController.hpp"
 #include "../../include/Protocol.hh"
 #include "../../include/ArcadeEnum.hpp"
