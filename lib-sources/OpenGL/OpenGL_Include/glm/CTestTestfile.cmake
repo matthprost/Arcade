45f32ee4891c72de3951761b64688096ce2cff84
Loïc Lopez <loic.lopez@epitech.eu>
2017-04-06 13:35:42 +0200

    delete opengl unused

--- lib-sources/OpenGL/OpenGL_Include/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prost_m/Downloads/glm/glm
-# Build directory: /home/prost_m/Downloads/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
